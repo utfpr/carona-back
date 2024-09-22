393d4a03e260b261b15a643a9ae84634c4cefb2a
WWteodoro <bostawwtr@outlook.com>
2024-09-22 20:14:28 -0300

    fix: chat

--- src/services/message/GetMessageByChatService.ts
+++ src/services/message/GetMessageByChatService.ts
@@ -4,7 +4,7 @@ import { IMesageRepository } from "../../interfaces/IMessageRepository";
 export class GetMessageByChatService{
     constructor(private messageRepo: IMesageRepository){}
     async execute({chatId}: IMessageGetByChatRequest): Promise<IMessage[]>{
-        const result = await this.messageRepo.getByAuthor(chatId)
+        const result = await this.messageRepo.getByChat(chatId)
         return result
     }
 }
